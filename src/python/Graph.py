d81df2c00a7112270adaca019bc32debecf5778c
LGrant7 <lukasgrant3102@gmail.com>
2022-10-10 01:46:06 -0500

    GUI Library converted from TKinter to QtPy
    converted gui to qtpy. Changed graph class to use qtpy methods instead of tkinter. Graph is now a QWidget and uses a painter object to create the lines, points, and text

--- src/python/Graph.py
+++ src/python/Graph.py
@@ -1,10 +1,13 @@
 # Graph class created by Lukas Grant
 # Imports
 from tkinter import *
-from qtpy import QtGui, QtCore
+import sys
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
 
 
-class Graph:
+class Graph(QWidget):
     # Graph properties
     x_title = "X-Axis"
     y_title = "Y-Axis"
@@ -23,26 +26,30 @@ class Graph:
 
     # Creates a point on the given canvas at the given coordinates
     @staticmethod
-    def create_point(param_canvas, x, y):
-        param_canvas.create_oval(x-2, y-2, x+2, y+2, fill='black')
+    def create_point(painter, x, y):
+        painter.drawEllipse(x - 2, y - 2, 4, 4)
 
-    def __init__(self, window, xvalues, yvalues):
+    def __init__(self, xvalues, yvalues):
+        super().__init__()
         self.width = 600
         self.height = 400
 
-        self.canvas = Canvas(window, width=self.width, height=self.height, bg=self.background_color)
-        print('canvas created with height = ' + str(self.height))
         self.x_values = xvalues
         self.y_values = yvalues
 
+    def paintEvent(self, event):
+        # Create painter object
+        painter = QPainter(self)
+        painter.setPen(Qt.black)
+
         # x-axis text
-        self.canvas.create_text(self.width / 2, self.height - 15, text=self.x_title, font=self.font)
+        painter.drawText(self.width / 2, self.height - 15, self.x_title)
 
         # Axis lines
         # y line
-        self.canvas.create_line(100, 50, 100, self.height - 100)
+        painter.drawLine(100, 50, 100, self.height - 100)
         # x line
-        self.canvas.create_line(100, self.height - 100, self.width - 100, self.height - 100)
+        painter.drawLine(100, self.height - 100, self.width - 100, self.height - 100)
 
         y_axis_line_length = (self.height - 100) - 50
         x_axis_line_length = (self.width - 100) - 100
@@ -71,26 +78,24 @@ class Graph:
             multiplier = i * len(self.x_values) / (len(self.x_values) - 1)
             linexpos = (multiplier * (x_axis_line_length / len(self.y_values))) + y_axis_offset
             x_coord_list.append(linexpos)
-            self.canvas.create_line(linexpos, x_axis_offset - 5, linexpos, x_axis_offset + 5)
-            self.canvas.create_text(linexpos, x_axis_offset + 12, text=str(self.x_values[i]), font='arial 7')
+            painter.drawLine(linexpos, x_axis_offset - 5, linexpos, x_axis_offset + 5)
+            painter.drawText(linexpos - 3, x_axis_offset + 16, str(self.x_values[i]))
 
         # Create lines and labels for y-axis
         for i in range(len(self.y_values)):
             multiplier = i * len(self.y_values) / (len(self.y_values) - 1)
             lineypos = (multiplier * (y_axis_line_length / len(self.x_values))) + 50
-            self.canvas.create_line(y_axis_offset - 5, lineypos, y_axis_offset + 5, lineypos)
-            self.canvas.create_text(y_axis_offset - 12, lineypos, text=str(list_prices[len(self.y_values) - (i + 1)]),
-                                    font='arial 7')
+            painter.drawLine(y_axis_offset - 5, lineypos, y_axis_offset + 5, lineypos)
+            painter.drawText(y_axis_offset - 25, lineypos + 4, str(list_prices[len(self.y_values) - (i + 1)]))
 
         # Loops through and creates points at price values
         for i in range(len(self.y_values)):
             y_coord_list.append(((1 - (self.y_values[i] / y_max)) * y_axis_line_length) + 50)
-            self.create_point(self.canvas, x_coord_list[i], y_coord_list[i])
+            self.create_point(painter, x_coord_list[i], y_coord_list[i])
 
         for i in range(len(x_coord_list) - 1):
-            self.canvas.create_line(x_coord_list[i], y_coord_list[i], x_coord_list[i + 1], y_coord_list[i + 1])
+            painter.drawLine(x_coord_list[i], y_coord_list[i], x_coord_list[i + 1], y_coord_list[i + 1])
 
-        self.canvas.pack()
 
     # Returns the list of x_values
     def get_x_values(self):
@@ -102,6 +107,3 @@ class Graph:
 
     def get_height(self):
         return self.height
-
-    def myfunction(self):
-        self.canvas.create_line(0, 100, 150, 200)
--- src/python/Main.py
+++ src/python/Main.py
@@ -1,11 +1,47 @@
 # Imports
 from tkinter import *
 from src.python.Graph import Graph
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
 
+
+# Create app
+app = QApplication([])
+app.setStyle('Fusion')
+window = Graph(xvalues=[0, 1, 2, 3, 4], yvalues=[100, 0, 10, 50, 33])
+
+"""
+# Set window layout
+layout = QVBoxLayout()
+window.setLayout(layout)
+
+# Create button widgets
+topButton = QPushButton('Top')
+
+
+# Define button functions
+def print_test():
+    print("this button works!")
+
+
+# Connect functions to buttons
+topButton.clicked.connect(print_test)
+
+# Add widgets to window
+layout.addWidget(topButton)
+"""
+
+
+# Show window
+window.show()
+app.exec()
+
+"""
 # Create main window
 window = Tk()
 window.title('Main')
 window.configure(width=1000, height=800)
+
+
 
 
 # Create a graph object
@@ -16,3 +52,4 @@ button.pack()
 
 
 window.mainloop()
+"""
