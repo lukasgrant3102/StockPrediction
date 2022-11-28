8e7391350c6145efbdef0552702ec9971220d709
LGrant7 <75303904+lukasgrant3102@users.noreply.github.com>
2022-11-28 11:49:07 -0600

    Groups of increasing and decreasing

--- src/python/Main.py
+++ src/python/Main.py
@@ -12,32 +12,32 @@ decreasing_list = increasing_list[::-1]
 random_list = [1, 2, 3, 2, 5, 7, 5, 4, 2, 6]
 # print(list(split(increasing_list, 4)))
 
-###
-def increasing(list_a):
-    current_value = list_a[0]
-    for i in range(len(list_a)):
-        if list_a[i] > current_value:
-            # Continue loop until not increasing
-            current_value = list_a[i]
-        elif list_a[i] <= current_value and i > 0 and len(list_a) - len(list_a[i:]) > 1:
-            print('stop and warn')
-            print('increased from index 0 value ' + str(list_a[0]) + ' to index ' + str(i-1) + ' value ' + str(list_a[i-1]))
-            # create list after stop point to end
-            print(list_a[i:])
-            increasing(list_a[i:])
-            break
+
+def grouping(list_a):
+    # Initial variables
+    main = []
+    temp_group = []
+    is_increasing = True
+
+    # Check first two values to see if list begins by increasing
+    if list_a[0] > list_a[1]:
+        is_increasing = False
+
+    # Check values in list in pairs
+    for current, after in zip(list_a, list_a[1:]):
+        temp_group.append(current)
+
+        # Create group and add to main list if the list swaps increasing/decreasing
+        if (after < current and is_increasing) or (after > current and not is_increasing):
+            main.append(temp_group)
+            temp_group = []
+            is_increasing = not is_increasing
+
+    print(main)
 
 
-def increasing_2(list_a):
-    current_value = list_a[0]
-    for i in range(len(list_a)):
-        if i > 0 and list_a[i] < current_value:
-            print(list_a[i:])
-            increasing_2(list_a[i:])
+grouping(random_list)
 
-
-
-increasing_2(random_list)
 # increasing(decreasing_list, 0, len(decreasing_list))
 # increasing(random_list, 0, len(random_list))
 
